13908090265dcd541c2d7fecb51f94e542176ba0
kristajg <krista.goralczyk@gmail.com>
2022-10-28 11:38:31 -0600

    Add active style to main nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,48 @@
+import { NavLink } from 'react-router-dom';
+
 function Navbar() {
+  const linkBaseClasses = 'nav-link px-2 text-secondary';
+  const linkStyles = isActive => {
+    return isActive ? `text-light ${linkBaseClasses}` : linkBaseClasses;
+  }
+
   return (
     <header className='text-bg-dark'>
       <div className='container'>
         <div className='d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start'>
           <ul className='nav col-12 col-lg-auto me-lg-auto justify-content-left mb-md-0'>
             <li>
-              <a className='nav-link px-2 text-secondary' href='/'>
+              <NavLink
+                className={({ isActive }) => linkStyles(isActive)}
+                to='/'
+                end
+              >
                 Home
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a className='nav-link px-2 text-secondary' href='/messaging'>
+              <NavLink
+                className={({ isActive }) => linkStyles(isActive)}
+                to='messaging'
+              >
                 Messaging
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a className='nav-link px-2 text-secondary' href='/voice'>
+              <NavLink
+                className={({ isActive }) => linkStyles(isActive)}
+                to='voice'
+              >
                 Voice
-              </a>
+              </NavLink>
             </li>
             <li>
-              <a className='nav-link px-2 text-secondary' href='/verify'>
+              <NavLink
+                className={({ isActive }) => linkStyles(isActive)}
+                to='verify'
+              >
                 Verify
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
